7ca43691cf5fd66f4af57d8c8c07fcb130fad068
TalebMaham <sidnas9@gmail.com>
2024-12-29 12:59:03 +0100

    structure de base de portfolio

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    if path.endswith('.js'):
+        return send_from_directory(app.static_folder, path, mimetype='application/javascript')
+    elif path.endswith('.css'):
+        return send_from_directory(app.static_folder, path, mimetype='text/css')
+    else:
+        return send_from_directory(app.static_folder, path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/chunk-YF4HMBQO.js
@@ -0,0 +1,1 @@
+import{k as a}from"./chunk-2MMOJ2K5.js";export{a as PortfolioModule};
